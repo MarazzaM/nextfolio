3d89c8561daaac0a7b6b9a6dd43b674657671fff
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-11-19 17:08:52 -0300

    updated desktop view (change overflow)

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ import { Typewriter } from 'typewriting-react';
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className={styles.container}>
     <div className={styles.hero}>
       <h1>Matías Marazza</h1>
-      <Typewriter words={['Front End Developer', 'React Developer', 'UI/UX Developer', 'Student', 'Next.js enthusiast']} 
+      <Typewriter words={['Front End Developer.', 'React Developer.', 'UI/UX Developer.', 'Student.', 'Next.js enthusiast.']} 
       typingSpeed={150}
       erasingSpeed={100}
       className={styles.type}
@@ -21,26 +21,39 @@ const Home: NextPage = () => {
       /> 
       <br />
       {/* prevent the page from jumping   */}
+      <div className={styles.img}>
       <Image 
       src={"/developer.svg"}
       alt="Developer vector"
       width={300}
       height={300}
-      className={styles.img}
       />
+      </div>
       <p className={styles.text}>
         An aspiring developer, who loves to bring imagination and creativity to
         projects. Always ready to learn and take on new challenges
       </p>
-    </div>
-    <div className={styles.spheres}>
+      <div className={styles.spheres}>
       <span className={styles.linkedin}><a href="https://www.linkedin.com/in/mat%C3%ADas-edgardo-marazza-cantero" target="_blank" rel="noopener noreferral"><BsLinkedin/></a></span>
       <span className={styles.github}><a href="https://github.com/MarazzaM"  target="_blank" rel="noopener noreferral"><BsGithub/></a></span>
       <span className={styles.mail}><a href="mailto:memarazza@gmail.com" rel="noopener noreferral" ><BsGoogle/></a></span>
     </div>
     <a href="/marazza-cv.pdf" download className={styles.neon}> See my Resume </a>
+    </div>
     
-    </>
+    <div className={styles.imgdesktop}>
+    <Image 
+      src={"/developer.svg"}
+      alt="Developer vector"
+      width={500}
+      height={500}
+      />
+      <br />
+      <span className={styles.text}>
+      Let's work together for a better future
+      </span>
+    </div>
+    </div>
   );
 };
 
