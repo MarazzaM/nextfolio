aed5d2c8e3e8a37916e981bf49cd74a9b84e823f
MarazzaM <marazza.matias@gmail.com>
2022-04-15 20:57:47 -0300

    added framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,29 @@ import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import SEO from '../components/SEO'
 import type { AppProps } from 'next/app'
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
 
   <Layout>
     <SEO />
     <Navbar />
+    <motion.div
+      key={router.route}
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}
+    >
   <Component {...pageProps} />
+  </motion.div>
   </Layout >
   
     )
